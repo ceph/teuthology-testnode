1bf863f36869b817915f8400aacfb66fb9e10323
Zack Cerza <zack@redhat.com>
2024-05-30 14:02:50 -0600

    Use Centos 9.Stream
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- testnode_start.sh
+++ testnode_start.sh
@@ -3,7 +3,11 @@ set -x
 cat /run/secrets/id_rsa.pub >> /root/.ssh/authorized_keys
 cat /run/secrets/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 chown ubuntu /home/ubuntu/.ssh/authorized_keys
-payload="{\"name\": \"$(hostname)\", \"machine_type\": \"testnode\", \"up\": true, \"locked\": false, \"os_type\": \"centos\", \"os_version\": \"8.stream\"}"
+. /etc/os-release
+if [ $ID = 'centos' ]; then
+    VERSION_ID=${VERSION_ID}.stream
+fi
+payload="{\"name\": \"$(hostname)\", \"machine_type\": \"testnode\", \"up\": true, \"locked\": false, \"os_type\": \"${ID}\", \"os_version\": \"${VERSION_ID}\"}"
 for i in $(seq 1 5); do
     echo "attempt $i"
     curl -v -f -d "$payload" http://paddles:8080/nodes/ && break
